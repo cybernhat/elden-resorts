0a58dcb0a47066e7a1390e91825df9f7923f2bdb
nhatdngo <nhatdngo@gmail.com>
2024-06-19 22:26:43 -0700

    sequelize setup

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+
